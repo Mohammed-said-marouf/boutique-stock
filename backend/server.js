baa4da446472489b8829b24319e98c6a13767524
Spicelevirus <mohammedsaidmarouf@gmail.com>
2026-07-28 08:41:34 +0100

    Augmenter la limite de taille JSON pour accepter les GIF en base64

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 const path = require('path');
 app.use('/uploads', require('express').static(path.join(__dirname, 'uploads')));
 
